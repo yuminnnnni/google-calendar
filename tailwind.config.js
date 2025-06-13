8d421e36c4dba4cbde86fc93ff65873810b258a7
yuminnnnni <ka09023@ajou.ac.kr>
2025-06-13 17:46:16 +0900

    chore: 전역 스타일 적용을 위한 main 및 tailwind 설정 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   plugins: [],
 }
